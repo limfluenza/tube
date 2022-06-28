5ef62a76803e3ca433be33663ee1d0ee2f16ba3a
lim <limfluenza@nvaer.com>
2022-06-28 22:17:25 +0900

    2day

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.send("<h1> Homepage </h1>");
+};
+const handleLogin = (req, res) => {
+  return res.send("<h1> ID and password </h1>");
+};
+const handleAbout = (req, res) => {
+  return res.send("<h1> About? </h1>");
+};
+const handleContact = (req, res) => {
+  return res.send("<h1> Contact! </h1>");
+};
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+app.get("/about", handleAbout);
+app.get("/contact", handleContact);
+
+const handleListening = () =>
+  console.log(`Server listenting on port http://localhost${PORT} `);
+
+app.listen(4000, handleListening);
