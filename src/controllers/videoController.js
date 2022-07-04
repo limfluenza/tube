b09e50ab9b943b5b8bf3f1046e17e0f44ece5456
lim <limfluenza@nvaer.com>
2022-07-05 05:18:25 +0900

    6day

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,16 +1,10 @@
-export const trending = (req, res) =>
-  res.send(
-    "<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>"
-  );
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+
 export const see = (req, res) => {
-  return res.send(
-    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Watch video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
-  );
+  return res.render("watch");
 };
 export const edit = (req, res) => {
-  return res.send(
-    `<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Edit video #${req.params.id}</h1><footer>&copy;2021 Wetube -  All rights reserved</footer></body></html>`
-  );
+  res.render("edit");
 };
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
